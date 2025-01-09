1e0d90d6c91128c6a30feb17c9bf607053958ed4
Alexander Kingdon <kingdon.alexander@gmail.com>
2025-01-09 10:07:28 +0100

    Refactor Day1 again

--- Day1/Day1.fs
+++ Day1/Day1.fs
@@ -1,26 +1,30 @@
 namespace Aoc.Day1
 
 module Part1 =
-    let removeSign a b =
-        match a with
-        | a when a - b < 0 -> (a - b) * -1
-        | _ -> a - b
-    
-    let getDifferencesBetween (list1: int list) (list2: int list) =
-        List.map2 (fun item item2 -> removeSign item item2) list1 list2
-
-    let addThemTogether list =
-        List.reduce (fun current next -> current + next) list
-
-    let doAllThatStuff list1 list2 =
-        list1 |> List.sort |> getDifferencesBetween (List.sort list2) |> addThemTogether
+    let getTotalDistance list1 list2 =
+        list2
+        |> List.sort
+        |> List.zip (List.sort list1)
+        |> List.map (fun (item1,item2) -> item1 - item2 |> abs)
+        |> List.reduce (fun curr next -> curr + next)
 
 module Part2 =
+    let (|MatchingPair|NoMatchingPair|) (left, right) =
+        if left = right then MatchingPair else NoMatchingPair
+
+    let getIncrement (left, right) =
+        match (left, right) with
+        | MatchingPair -> 1
+        | NoMatchingPair -> 0
+
     let numberOfOccurrencesInList (inputNumber: int) (list: int list) =
-        List.fold (fun total curr -> if curr = inputNumber then total + 1 else total) 0 list
+        List.fold (fun total curr ->
+            (inputNumber, curr)
+            |> getIncrement
+            |> (+) total) 0 list
 
-    let doAllThatStuff list1 list2 =
-        let occurrences = List.map (fun i -> numberOfOccurrencesInList i list2) list1
-        let scores = List.map2 (fun i i2 -> i * i2) list1 occurrences
-
-        List.reduce (fun curr next -> curr + next) scores
+    let doAllThatStuff (list1: int list) list2 =
+        list1
+        |> List.map (fun item -> numberOfOccurrencesInList item list2)
+        |> List.map2 (fun i i2 -> i * i2) list1
+        |> List.reduce (fun curr next -> curr + next)
