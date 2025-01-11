d789d7df27b98d212ee36cbb3cc7b8f50f2ed60e
Alexander Kingdon <serbern87@gmail.com>
2025-01-11 12:05:25 +0100

    Day 2 part 2 doodling

--- Day2/Day2.fs
+++ Day2/Day2.fs
@@ -5,29 +5,69 @@ module Part1 =
     | Increasing
     | Decreasing
 
+    type Distance =
+    | TooGreat
+    | Acceptable
+    | Equal
+
     let getDirection (a, b) =
         match (a, b) with
         | a,b when a < b -> Increasing
         | _ -> Decreasing
 
-    let (|InvalidDistance|ValidDistance|) (first, second) =
-        let res = (first - second) |> abs
-        if res > 3 || res < 1 then InvalidDistance else ValidDistance
+    let getDistance (a, b) =
+        let distance = a - b |> abs
 
-    let validDistance (first, second) =
-        match (first, second) with
-        | ValidDistance -> true
-        | InvalidDistance -> false
+        match distance with
+        | distance when distance > 3 -> TooGreat
+        | distance when distance > 0 -> Acceptable
+        | _ -> Equal
+
+    let initialDirection list = list |> List.head |> getDirection
+
+    let validDistance (a, b) =
+        match (a, b) |> getDistance with
+        | TooGreat | Equal -> false
+        | _ -> true
+
+    let allDistancesAreValid list =
+        list |> List.forall (fun x -> x |> validDistance)
+
+    let allDirectionsMatch list =
+        let firstDirection = list |> initialDirection
+        list |> List.tail |> List.forall (fun x -> x |> getDirection = firstDirection)
 
     let reportIsSafe report =
         let pairs = report |> List.pairwise
-        let initialDirection = pairs |> List.head |> getDirection
 
-        pairs |> List.forall (fun x ->
-            x |> getDirection = initialDirection &&
-            x |> validDistance)
+        pairs |> allDistancesAreValid && pairs |> allDirectionsMatch
 
     let doSomeStuff reports =
         reports
         |> List.filter (fun report -> report |> reportIsSafe)
+        |> List.length
+
+module Part2 =
+    let getEqualDistancePair list =
+        list |> List.filter (fun x -> x |> Part1.getDistance = Part1.Equal)
+
+    let reversePairwise list =
+        list |> List.map (fun x -> x |> fst)
+
+    let reportIsSafeWithProblemDampener report =
+        let pairs = report |> List.pairwise
+        let firstDirection = pairs |> Part1.initialDirection
+
+        let directionErrorsRemoved = pairs |> List.filter (fun x -> x |> Part1.getDirection = firstDirection)
+        let distanceErrorsRemoved = directionErrorsRemoved |> List.filter (fun x -> x |> Part1.validDistance)
+
+        let newReport = distanceErrorsRemoved |> reversePairwise
+        let newReportPasses = newReport |> Part1.reportIsSafe
+        let newReportHadOneErrorRemoved = (newReport |> List.length) - (report |> List.length) |> abs = 1
+
+        newReportPasses && newReportHadOneErrorRemoved
+
+    let doSomeStuff reports =
+        reports
+        |> List.filter (fun report -> report |> reportIsSafeWithProblemDampener)
         |> List.length
