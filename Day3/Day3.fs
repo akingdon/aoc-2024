801a5551e81df55ac42cdbe4ad03cb9c69d9b135
Alexander Kingdon <serbern87@gmail.com>
2025-01-18 14:13:11 +0100

    Finish Day 3 part 2

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -5,9 +5,9 @@ open System.Text.RegularExpressions
 module Part1 =
     let getMulFromInput = "mul\(\d+,\d+\)"
     let getNumbersFromMul = "(\d+,\d+)"
-    let fullRegex = "(mul\(\d*\,\d*\))|(do\(\))|(don\'t\(\))"
+    let fullRegex = "(?'mul'mul\(\d*\,\d*\))|(?'do'do\(\))|(?'dont'don\'t\(\))"
 
-    let matches str = Regex.Matches(str, getMulFromInput)
+    let matches str pattern = Regex.Matches(str, pattern)
 
     let getNumbers (str: string) =
         let res = ',' |> str.Split
@@ -16,9 +16,65 @@ module Part1 =
 
     let multiplyThem (a, b) = a * b
 
-    let pairs (collection: MatchCollection) = 
+    let pairs (collection: MatchCollection) =
         collection
         |> Seq.map (fun m -> Regex.Matches(m.Value, getNumbersFromMul))
         |> Seq.concat
         |> Seq.map (fun x -> x.Value |> getNumbers |> multiplyThem)
         |> Seq.reduce (fun current next -> current + next)
+
+module Part2 =
+    type Result =
+        | Mul
+        | Do
+        | Don't
+
+    let successfulMatch name (group: Group) = group.Name = name && group.Success
+
+    let (|Mul|_|) (m: Match) =
+        if (m.Groups |> Seq.exists (successfulMatch "mul")) then
+            Some Mul
+        else
+            None
+
+    let (|Do|_|) (m: Match) =
+        if (m.Groups |> Seq.exists (successfulMatch "do")) then
+            Some Do
+        else
+            None
+
+    let (|Don't|_|) (m: Match) =
+        if (m.Groups |> Seq.exists (successfulMatch "dont")) then
+            Some Don't
+        else
+            None
+
+    let matchType (m: Match) =
+        match m with
+        | Mul -> Mul
+        | Do -> Do
+        | _ -> Don't
+
+    let allMatches = Part1.fullRegex |> Part1.matches Inputs.theWholeThing
+
+    let getMatchesUntil typeToMatch =
+        allMatches |> Seq.takeWhile (fun m -> m |> matchType = typeToMatch)
+
+    let mulStringToNumbers (mulMatch: Match) =
+        Regex.Match(mulMatch.Value, Part1.getNumbersFromMul).Value
+        |> Part1.getNumbers
+        |> Part1.multiplyThem
+
+    let rec getTotal (coll: Match list) totalValue =
+        match coll with
+        | [] -> totalValue
+        | head :: tail when head |> matchType = Mul -> getTotal tail (totalValue + (head |> mulStringToNumbers))
+        | head :: tail when head |> matchType = Do -> getTotal tail totalValue
+        | head :: tail when head |> matchType = Don't ->
+            let nextMult = tail |> Seq.tryFindIndex (fun item -> item |> matchType = Do)
+
+            if nextMult.IsSome then
+                (getTotal tail[nextMult.Value ..] totalValue)
+            else
+                (getTotal [] totalValue)
+        | _ -> totalValue
--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
 ﻿open Aoc
+open Aoc.Day3
 
 let d1p1 = Day1.Part1.getTotalDistance Day1.Inputs.list1 Day1.Inputs.list2
 printfn $"Day 1 Part 1 result: {d1p1}"
@@ -14,5 +15,8 @@ let d2p2 = Day2.Part2.doSomeStuff Day2.Inputs.listOfLists
 printfn $"Day 2 Part 2 result: {d2p2}. Totals: {d2p1 + d2p2}"
 
 
-let d3p1 = Day3.Inputs.theWholeThing |> Day3.Part1.matches |> Day3.Part1.pairs
+let d3p1 = Day3.Part1.getMulFromInput |> Day3.Part1.matches Inputs.theWholeThing |> Day3.Part1.pairs
 printfn $"Day 3 Part 1 result: {d3p1}"
+
+let d3p2 = Day3.Part2.getTotal (Day3.Part2.allMatches |> List.ofSeq) 0
+printfn $"Day 3 Part 2 result: {d3p2}"
