f7be83b33b391037664cfcca4a21b556c5669ce3
Alexander Kingdon <kingdon.alexander@gmail.com>
2025-01-21 21:48:23 +0100

    Day 4 part 1

---
+++ Day4/Day4.fs
@@ -0,0 +1,93 @@
+namespace Aoc.Day4
+
+module Part1 =
+
+    let addTogether acc curr = acc + curr
+    let isTrue x = x = true
+
+    let checkDown (strs: string list) pos =
+        strs[0].[pos] = 'X'
+        && strs[1].[pos] = 'M'
+        && strs[2].[pos] = 'A'
+        && strs[3].[pos] = 'S'
+
+    let checkUp (strs: string list) pos =
+        strs[0].[pos] = 'S'
+        && strs[1].[pos] = 'A'
+        && strs[2].[pos] = 'M'
+        && strs[3].[pos] = 'X'
+
+    let checkUpsAndDowns (strs: string list) =
+        [ 0 .. strs[0].Length - 1 ]
+        |> List.map (fun x -> (checkDown strs x) || (checkUp strs x))
+        |> List.filter isTrue
+        |> List.length
+
+    let checkForwards (str: string) pos =
+        str[pos] = 'X'
+        && str[pos + 1] = 'M'
+        && str[pos + 2] = 'A'
+        && str[pos + 3] = 'S'
+
+    let checkBackwards (str: string) pos =
+        str[pos] = 'S'
+        && str[pos + 1] = 'A'
+        && str[pos + 2] = 'M'
+        && str[pos + 3] = 'X'
+
+    let checkForeAndAft (str: string) =
+        [ 0 .. str.Length - 4 ]
+        |> List.map (fun y -> (checkForwards str y) || (checkBackwards str y))
+        |> List.filter isTrue
+        |> List.length
+
+    let checkDiagonalNWToSE (strs: string list) pos =
+        strs[0].[pos] = 'X'
+        && strs[1].[pos + 1] = 'M'
+        && strs[2].[pos + 2] = 'A'
+        && strs[3].[pos + 3] = 'S'
+
+    let checkDiagonalNEToSW (strs: string list) pos =
+        strs[0].[pos + 3] = 'X'
+        && strs[1].[pos + 2] = 'M'
+        && strs[2].[pos + 1] = 'A'
+        && strs[3].[pos] = 'S'
+
+    let checkDiagonalSWToNE (strs: string list) pos =
+        strs[3].[pos] = 'X'
+        && strs[2].[pos + 1] = 'M'
+        && strs[1].[pos + 2] = 'A'
+        && strs[0].[pos + 3] = 'S'
+
+    let checkDiagonalSEToNW (strs: string list) pos =
+        strs[3].[pos + 3] = 'X'
+        && strs[2].[pos + 2] = 'M'
+        && strs[1].[pos + 1] = 'A'
+        && strs[0].[pos] = 'S'
+
+    let checkAllDiagonals (strs: string list) =
+        [ 0 .. strs[0].Length - 4 ]
+        |> List.map (fun pos ->
+            [ (checkDiagonalNWToSE strs pos)
+              (checkDiagonalNEToSW strs pos)
+              (checkDiagonalSWToNE strs pos)
+              (checkDiagonalSEToNW strs pos) ]
+            |> List.filter isTrue
+            |> List.length)
+        |> List.reduce addTogether
+
+    let doAFullRunOfFours (strs: string list) =
+        let upsAndDowns = checkUpsAndDowns strs[0..3]
+        let diagonals = checkAllDiagonals strs[0..3]
+
+        upsAndDowns + diagonals
+
+    let doAFullRunOfAlls (strs: string list) =
+        let multipleLiners =
+            [ 0 .. strs[0].Length - 4 ]
+            |> List.map (fun a -> doAFullRunOfFours strs[a .. a + 3])
+            |> List.reduce addTogether
+
+        let singleLiners = strs |> List.map checkForeAndAft |> List.reduce addTogether
+
+        multipleLiners + singleLiners
--- Program.fs
+++ Program.fs
@@ -1,5 +1,4 @@
 ﻿open Aoc
-open Aoc.Day3
 
 let d1p1 = Day1.Part1.getTotalDistance Day1.Inputs.list1 Day1.Inputs.list2
 printfn $"Day 1 Part 1 result: {d1p1}"
@@ -15,8 +14,12 @@ let d2p2 = Day2.Part2.doSomeStuff Day2.Inputs.listOfLists
 printfn $"Day 2 Part 2 result: {d2p2}. Totals: {d2p1 + d2p2}"
 
 
-let d3p1 = Day3.Part1.getMulFromInput |> Day3.Part1.matches Inputs.theWholeThing |> Day3.Part1.pairs
+let d3p1 = Day3.Part1.getMulFromInput |> Day3.Part1.matches Day3.Inputs.theWholeThing |> Day3.Part1.pairs
 printfn $"Day 3 Part 1 result: {d3p1}"
 
 let d3p2 = Day3.Part2.getTotal (Day3.Part2.allMatches |> List.ofSeq) 0
 printfn $"Day 3 Part 2 result: {d3p2}"
+
+
+let d4p1 = Day4.Part1.doAFullRunOfAlls Day4.Inputs.actual
+printfn $"Day 4 Part 1 result: {d4p1}"
